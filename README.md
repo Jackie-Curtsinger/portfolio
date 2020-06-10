1bd3209c9b403db3b8103dc6872060ada7011fac
Jackie Curtsinger <jackie_curtsinger@yahoo.com>
2020-06-10 13:50:35 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# React Portfolio
+Minimal Portfolio built with React. 
---
+++ src/components/data.js
@@ -0,0 +1,83 @@
+export default {
+
+  name: "Jackie Curtsinger",
+
+
+  contactEmail: "www.jackie_curtsinger@yahoo.com",
+
+
+  landingPageName: "Jackie Curtsinger",
+
+  
+  landingPagePara:
+    "Let me use my passion of skill aquisition to help you succeed.",
+
+  
+  landingPageImage: "assets/jax.jpg",
+
+  
+  projects: [
+    {
+      id: 1, 
+      title: "Bust a move fitness", //Project Title 
+      // Image Size Should be (300X300 px)
+      imageSrc: "https://via.placeholder.com/300",
+      //Project URL
+      url: "http://google.com/"
+    },
+    {
+      id: 2, 
+      title: "Project Two",
+      imageSrc: "https://via.placeholder.com/300",
+      url: "http://google.com/"
+    },
+    {
+      id: 3, 
+      title: "Project Three",
+      imageSrc: "https://via.placeholder.com/300",
+      url: "http://google.com/"
+    },
+    {
+      id: 4, 
+      title: "Project Four",
+      imageSrc: "https://via.placeholder.com/300",
+      url: "http://google.com/"
+    },
+    {
+      id: 5, 
+      title: "Project Five",
+      imageSrc: "https://via.placeholder.com/300",
+      url: "http://google.com/"
+    },
+    {
+      id: 6, 
+      title: "Project Six",
+      imageSrc: "https://via.placeholder.com/300",
+      url: "http://google.com/"
+    }
+    
+  ],
+
+  // Contact Section
+  contactPara:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry 's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  social: [
+    // Add Or Remove The Link Accordingly
+    {
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/"
+    },
+    {
+      name: "Github",
+      url: "https://github.com/Jackie-Curtsinger?tab=repositories"
+    },
+    {
+      name: "Github",
+      url: "https://github.com/Jackie-Curtsinger?tab=repositories"
+    },
+  ],
+
+  // About Page
+  aboutTitle: "I gain skills like the Tardis aquires Doctors",
+  aboutPara:"Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry 's standard dummy text eversince the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+};
